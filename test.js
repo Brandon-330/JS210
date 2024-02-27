50388841041851d8df725d4427ddfb38c34175ca
Brandon Lima <brandonsamlima@gmail.com>
2024-02-27 18:58:20 -0500

    First commit

---
+++ test.js
@@ -0,0 +1,16 @@
+let numerator = 10;
+const DENOMINATOR = 2;
+let answer = numerator / DENOMINATOR;
+let increment = 1;
+let start = increment;
+let end;
+let difference;
+increment += 1;
+increment = increment + 1;
+increment++;
+increment++;
+increment++;
+end = increment;
+difference = end - start;
+answer = (11 + 31) * 3;
+console.log(answer)
